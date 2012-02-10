00dad7900a9508dcc88ba52361981e5e5f015ec7
Christopher Pickslay <cpickslay@twobitlabs.com>
2012-02-09 23:44:09 -0800

    whitespace

--- Tests/Unit Tests/SomeUnitTest.m
+++ Tests/Unit Tests/SomeUnitTest.m
@@ -13,22 +13,19 @@
 
 @implementation SomeUnitTest
 
-- (void)setUp
-{
+-(void)setUp {
     [super setUp];
     
     // Set-up code here.
 }
 
-- (void)tearDown
-{
+- (void)tearDown {
     // Tear-down code here.
     
     [super tearDown];
 }
 
-- (void)testExample
-{
+- (void)testExample {
     STFail(@"Unit tests are not implemented yet in Unit Tests");
 }
 
