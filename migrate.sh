bab5e3eb1c0f99f94d0e79cdfc9790c37701514c
Christopher Pickslay <cpickslay@twobitlabs.com>
2012-02-13 11:20:48 -0800

    added dev/debug plists to migration script

--- migrate.sh
+++ migrate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm -fr Tests Application Application.xcodeproj 
-
 echo -n "Rename project to what? "
 read PROJECT_NAME
 
+# remove previously migrated directories
+rm -fr Libraries Tests Application Application.xcodeproj
 rm -fr $PROJECT_NAME $PROJECT_NAME.xcodeproj
 
 rm -rf /tmp/iOSXcodeStarterProject
@@ -18,12 +18,16 @@ sed -i "" s/Application.app/$PROJECT_NAME.app/g Application.xcodeproj/project.pb
 sed -i "" s/Application\\/Application-Info.plist/$PROJECT_NAME\\/$PROJECT_NAME-Info.plist/g Application.xcodeproj/project.pbxproj
 sed -i "" s/Application\\/Application-Prefix.pch/$PROJECT_NAME\\/$PROJECT_NAME-Prefix.pch/g Application.xcodeproj/project.pbxproj
 sed -i "" s/Application-Info.plist/$PROJECT_NAME-Info.plist/g Application.xcodeproj/project.pbxproj
+sed -i "" s/Application-Dev-Info.plist/$PROJECT_NAME-Info.plist/g Application.xcodeproj/project.pbxproj
+sed -i "" s/Application-Debug-Info.plist/$PROJECT_NAME-Info.plist/g Application.xcodeproj/project.pbxproj
 sed -i "" s/Application-Prefix.pch/$PROJECT_NAME-Prefix.pch/g Application.xcodeproj/project.pbxproj
 sed -i "" "s/= Application/= $PROJECT_NAME/g" Application.xcodeproj/project.pbxproj
 sed -i "" s/Application/$PROJECT_NAME/g Vendorfile
 
 mv -f Application $PROJECT_NAME
 mv $PROJECT_NAME/Application-Info.plist $PROJECT_NAME/$PROJECT_NAME-Info.plist
+mv $PROJECT_NAME/Application-Dev-Info.plist $PROJECT_NAME/$PROJECT_NAME-Dev-Info.plist
+mv $PROJECT_NAME/Application-Debug-Info.plist $PROJECT_NAME/$PROJECT_NAME-Debug-Info.plist
 mv $PROJECT_NAME/Application-Prefix.pch $PROJECT_NAME/$PROJECT_NAME-Prefix.pch
 mv -f Application.xcodeproj $PROJECT_NAME.xcodeproj
 
