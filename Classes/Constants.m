4bd1f22b8c605b2dafa573bd8191390cc33ed603
Christopher Pickslay <cpickslay@twobitlabs.com>
2012-02-13 15:49:59 -0800

    added localytics key

--- Classes/Constants.m
+++ Classes/Constants.m
@@ -14,5 +14,6 @@ NSString* const FLURRY_KEY = @"FlurryKey";
 NSString* const TESTFLIGHT_KEY = @"TestFlightKey";
 NSString* const APSALAR_KEY = @"ApSalarKey";
 NSString* const APSALAR_SECRET = @"ApSalarSecret";
+NSString* const LOCALYTICS_KEY = @"LocalyticsKey";
 
 @end
