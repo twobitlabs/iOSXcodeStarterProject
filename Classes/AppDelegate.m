2427fecc83b599b10b35c1aa74f1781cacf6a2fc
Christopher Pickslay <cpickslay@twobitlabs.com>
2012-02-13 15:49:39 -0800

    added analyticskit config

--- Classes/AppDelegate.m
+++ Classes/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AnalyticsKitTestFlightProvider.h"
 #import "AnalyticsKitFlurryProvider.h"
 #import "AnalyticsKitApsalarProvider.h"
+#import "AnalyticsKitLocalyitcsProvider.h"
 
 @interface AppDelegate ()
 
@@ -21,7 +22,7 @@
 @synthesize window = _window;
 
 -(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
+    [self configureAnalyticsWithOptions:launchOptions];
     return YES;
 }
 	
@@ -34,34 +35,33 @@ void analyticsExceptionHandler(NSException *exception) {
 
 -(void)configureAnalyticsWithOptions:(NSDictionary *)launchOptions {
 #if (!TARGET_IPHONE_SIMULATOR)
+    NSMutableArray *providers = [NSMutableArray array];
+
     NSString *flurryKey = [[NSBundle mainBundle] objectForInfoDictionaryKey:FLURRY_KEY];
+    [providers addObject:[[AnalyticsKitFlurryProvider alloc] initWithAPIKey:flurryKey]];
     
     NSString *apsalarKey = [[NSBundle mainBundle] objectForInfoDictionaryKey:APSALAR_KEY];
     NSString *apsalarSecret = [[NSBundle mainBundle] objectForInfoDictionaryKey:APSALAR_SECRET];
+    [providers addObject:[[AnalyticsKitApsalarProvider alloc] initWithAPIKey:apsalarKey andSecret:apsalarSecret andLaunchOptions:launchOptions]]; 
+    
+    NSString *localyticsKey = [[NSBundle mainBundle] objectForInfoDictionaryKey:LOCALYTICS_KEY];
+    [providers addObject:[[AnalyticsKitLocalyitcsProvider alloc] initWithAPIKey:localyticsKey]]; 
+    
+#ifdef DEBUG
+    NSString *testFlightKey = [[NSBundle mainBundle] objectForInfoDictionaryKey:TESTFLIGHT_KEY];
+    [providers addObject:[[AnalyticsKitTestFlightProvider alloc] initWithAPIKey:testFlightKey]];
 
-    NSString *testFlightKey = [[NSBundle mainBundle] objectForInfoDictionaryKey:TESTFLIGHT_KEY]
-
-    NSArray *loggers = [NSArray arrayWithObjects:
-                        [[AnalyticsKitFlurryProvider alloc] initWithAPIKey:key],
-                        [[AnalyticsKitApsalarProvider alloc] initWithAPIKey:apsalarKey andSecret:apsalarSecret andLaunchOptions:launchOptions], 
-                        [[AnalyticsKitTestFlightProvider alloc] initWithAPIKey:testFlightKey],
-                        nil];
-    [AnalyticsKit initializeLoggers:loggers];
-#else
+#endif
+    
+    [AnalyticsKit initializeLoggers:providers];
+#endif
 
     //initialize AnalyticsKit to send messages to Flurry and TestFlight
     NSSetUncaughtExceptionHandler(&analyticsExceptionHandler);
     
-    //Flurry: Track iOS version so we know when we can drop support for older versions
+    // Track iOS version so we know when we can drop support for older versions
     NSDictionary *versDict = [NSDictionary dictionaryWithObject:[[UIDevice currentDevice] systemVersion] forKey:@"version"];
-    [AnalyticsKit logEvent:@"App Started - All" withProperties:versDict];
-    if (kCFCoreFoundationVersionNumber < 550.32) {
-        [AnalyticsKit logEvent:@"App Started - 3" withProperties:versDict];
-    } else if (kCFCoreFoundationVersionNumber < 600.0) {
-        [AnalyticsKit logEvent:@"App Started - 4" withProperties:versDict];
-    } else {
-        [AnalyticsKit logEvent:@"App Started - 5" withProperties:versDict];
-    }    
+    [AnalyticsKit logEvent:@"App Started" withProperties:versDict];
 }
 
 @end
