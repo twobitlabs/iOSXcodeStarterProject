e202b2a6ef8a103fbb3251ac1c3d2f0cd3183646
Todd Huss <thuss@gabrito.com>
2012-07-08 18:58:51 -0700

    Added navigation controller to iPhone storyboard

--- Classes/ViewController.m
+++ Classes/ViewController.m
@@ -21,6 +21,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"nav controller is %@", self.navigationController);
 }
 
 - (void)viewDidUnload
